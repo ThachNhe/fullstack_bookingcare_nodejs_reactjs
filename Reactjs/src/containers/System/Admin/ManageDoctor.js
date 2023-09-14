2e3d07c89a2f59fd5cfe6ce3bad68190cfd60e23
ThachNhe <thachdinh110902@gmail.com>
2023-09-14 21:36:32 +0700

    design UI create doctor'info and API for it

--- Reactjs/src/containers/System/Admin/ManageDoctor.js
+++ Reactjs/src/containers/System/Admin/ManageDoctor.js
@@ -1,193 +1,253 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import MarkdownIt from "markdown-it";
-import MdEditor from "react-markdown-editor-lite";
-import "react-markdown-editor-lite/lib/index.css";
-import "./ManageDoctor.scss";
-import Select from "react-select";
-import * as actions from "../../../store/actions";
-import { LANGUAGES } from "../../../utils";
-import { getDetailInfoDoctor } from "../../../services/userService";
-import { manageActions } from "../../../utils/constant";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+import 'react-markdown-editor-lite/lib/index.css';
+import './ManageDoctor.scss';
+import Select from 'react-select';
+import * as actions from '../../../store/actions';
+import { LANGUAGES } from '../../../utils';
+import { getDetailInfoDoctor } from '../../../services/userService';
+import { manageActions } from '../../../utils/constant';
+import { FormattedMessage } from 'react-intl';
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 class ManageDoctor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contentMarkdown: "",
-      contentHTML: "",
-      selectedOption: null,
-      description: "",
-      listDoctor: [],
-      hasOldData: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            //
+            contentMarkdown: '',
+            contentHTML: '',
+            selectedOption: null,
+            description: '',
+            listDoctor: [],
+            hasOldData: false,
+
+            //save doctor info
+            listPrice: [],
+            listPayment: [],
+            listProvince: [],
+            selectedPrice: '',
+            selectPayment: '',
+            selectProvince: '',
+            nameClinic: '',
+            addressClinic: '',
+            note: '',
+        };
+    }
+
+    componentDidMount() {
+        this.props.fetchAllDoctor();
+        this.props.getRequireDoctorInfo();
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.allDoctors !== this.props.allDoctors) {
+            let dataSelect = this.buildDataInPutSelect(this.props.allDoctors, 'USERS');
+            this.setState({
+                listDoctor: dataSelect,
+            });
+        }
+        if (prevProps.language !== this.props.language) {
+            let dataSelect = this.buildDataInPutSelect(this.props.allDoctors);
+            this.setState({
+                listDoctor: dataSelect,
+            });
+        }
+        if (prevProps.allRequiredDoctorInfo !== this.props.allRequiredDoctorInfo) {
+            console.log('data from redux : ', this.props.allRequiredDoctorInfo);
+            let { resPayment, resPrice, resProvince } = this.props.allRequiredDoctorInfo;
+            let dataSelectPrice = this.buildDataInPutSelect(resPrice);
+            let dataSelectPayment = this.buildDataInPutSelect(resPayment);
+            let dataSelectProvince = this.buildDataInPutSelect(resProvince);
+            this.setState({
+                listPrice: dataSelectPrice,
+                listPayment: dataSelectPayment,
+                listProvince: dataSelectProvince,
+            });
+        }
+    }
+    buildDataInPutSelect = (data, type) => {
+        let result = [];
+        let { language } = this.props;
+        if (data && data.length > 0) {
+            data.map((item, index) => {
+                let object = {};
+                let labelVi = type === 'USERS' ? `${item.lastName} ${item.firstName}` : item.valueVi;
+                let labelEn = type === 'USERS' ? `${item.firstName} ${item.lastName}` : item.valueEn;
+                object.label = language === LANGUAGES.VI ? labelVi : labelEn;
+                object.value = item.id;
+                result.push(object);
+            });
+        }
+
+        return result;
     };
-  }
+    handleEditorChange = ({ html, text }) => {
+        this.setState({
+            contentHTML: html,
+            contentMarkdown: text,
+        });
+    };
 
-  componentDidMount() {
-    this.props.fetchAllDoctor();
-  }
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevProps.allDoctors !== this.props.allDoctors) {
-      let dataSelect = this.buildDataInPutSelect(this.props.allDoctors);
-      this.setState({
-        listDoctor: dataSelect,
-      });
+    handleChangeSelect = async (selectedOption) => {
+        this.setState({ selectedOption });
+        let res = await getDetailInfoDoctor(selectedOption.value);
+        if (res && res.errCode === 0 && res.data && res.data.Markdown && res.data.Markdown.contentHTML) {
+            let markdown = res.data.Markdown;
+            this.setState({
+                contentHTML: markdown.contentHTML,
+                contentMarkdown: markdown.contentMarkdown,
+                description: markdown.description,
+                hasOldData: true,
+            });
+        } else {
+            this.setState({
+                contentHTML: '',
+                contentMarkdown: '',
+                description: '',
+                hasOldData: false,
+            });
+        }
+        console.log('res doctor : ', res);
+    };
+    handleOnchangeDescription = (event) => {
+        this.setState({
+            description: event.target.value,
+        });
+    };
+    handleSaveContentMarkdown = () => {
+        let { contentHTML, contentMarkdown, description, selectedOption, hasOldData } = this.state;
+        if (contentHTML && contentMarkdown && description && selectedOption && selectedOption.value) {
+            this.props.saveDetailDoctorAction({
+                contentHTML: contentHTML,
+                contentMarkdown: contentMarkdown,
+                description: description,
+                doctorId: selectedOption.value,
+                action: hasOldData === true ? manageActions.EDIT : manageActions.CREATE,
+            });
+        } else {
+            alert('Vui lòng điền thông tin');
+        }
+    };
+    render() {
+        let { selectedOption, description, hasOldData } = this.state;
+        // console.log('state : ', this.state.listPrice);
+        // console.log('state : ', this.state.listPayment);
+        // console.log('state : ', this.state.listProvince);
+        return (
+            <div className="manage-doctor-container">
+                <div className="manage-doctor-title">
+                    <FormattedMessage id="admin.manage-doctor.title" />
+                </div>
+                <div className="more-info">
+                    <div className="content-left">
+                        <label>
+                            {' '}
+                            <FormattedMessage id="admin.manage-doctor.select-doctor" />
+                        </label>
+                        <Select
+                            value={selectedOption}
+                            onChange={this.handleChangeSelect}
+                            options={this.state.listDoctor}
+                            placeholder={<div>Chọn bác sĩ</div>}
+                        />
+                    </div>
+                    <div className="content-right">
+                        <label>
+                            {' '}
+                            <FormattedMessage id="admin.manage-doctor.intro" />
+                        </label>
+                        <textarea
+                            className="form-control"
+                            onChange={(event) => this.handleOnchangeDescription(event)}
+                            value={description}
+                        >
+                            ThachNhe
+                        </textarea>
+                    </div>
+                </div>
+                <div className="more-info-extra row">
+                    <div className="col-4 form-group">
+                        <label>Chọn giá</label>
+                        <Select
+                            // value={selectedOption}
+                            // onChange={this.handleChangeSelect}
+                            options={this.state.listPrice}
+                            placeholder={<div>Chọn giá</div>}
+                        />
+                    </div>
+                    <div className="col-4 form-group">
+                        <label>Chọn phương thức thanh toán</label>
+                        <Select
+                            // value={selectedOption}
+                            // onChange={this.handleChangeSelect}
+                            options={this.state.listPayment}
+                            placeholder={'Chọn phương thức thanh toán'}
+                        />
+                    </div>
+                    <div className="col-4 form-group">
+                        <label>Chọn tỉnh thành </label>
+                        <Select
+                            // value={selectedOption}
+                            // onChange={this.handleChangeSelect}
+                            options={this.state.listProvince}
+                            placeholder={<div>Chọn tỉnh thành</div>}
+                        />
+                    </div>
+                    <div className="col-4 form-group">
+                        <label>Tên phòng khám</label>
+                        <input className="form-control" />
+                    </div>
+                    <div className="col-4 form-group">
+                        <label>Địa chỉ phòng khám</label>
+                        <input className="form-control" />
+                    </div>
+                    <div className="col-4 form-group">
+                        <label>Note</label>
+                        <input className="form-control" />
+                    </div>
+                </div>
+                <div className="manage-doctor-editor">
+                    <MdEditor
+                        style={{ height: '500px' }}
+                        renderHTML={(text) => mdParser.render(text)}
+                        onChange={this.handleEditorChange}
+                        value={this.state.contentMarkdown}
+                    />
+                </div>
+                <button
+                    className={hasOldData === true ? 'save-content-doctor' : 'create-content-doctor'}
+                    onClick={() => this.handleSaveContentMarkdown()}
+                >
+                    {hasOldData === true ? (
+                        <span>
+                            <FormattedMessage id="admin.manage-doctor.save" />
+                        </span>
+                    ) : (
+                        <span>
+                            <FormattedMessage id="admin.manage-doctor.add" />
+                        </span>
+                    )}
+                </button>
+            </div>
+        );
     }
-    if (prevProps.language !== this.props.language) {
-      let dataSelect = this.buildDataInPutSelect(this.props.allDoctors);
-      this.setState({
-        listDoctor: dataSelect,
-      });
-    }
-  }
-  buildDataInPutSelect = (data) => {
-    let result = [];
-    let { language } = this.props;
-    if (data && data.length > 0) {
-      data.map((item, index) => {
-        let object = {};
-        let labelVi = `${item.lastName} ${item.firstName}`;
-        let labelEn = `${item.firstName} ${item.lastName}`;
-        object.label = language === LANGUAGES.VI ? labelVi : labelEn;
-        object.value = item.id;
-        result.push(object);
-      });
-    }
-
-    return result;
-  };
-  handleEditorChange = ({ html, text }) => {
-    this.setState({
-      contentHTML: html,
-      contentMarkdown: text,
-    });
-  };
-
-  handleChangeSelect = async (selectedOption) => {
-    this.setState({ selectedOption });
-    let res = await getDetailInfoDoctor(selectedOption.value);
-    if (
-      res &&
-      res.errCode === 0 &&
-      res.data &&
-      res.data.Markdown &&
-      res.data.Markdown.contentHTML
-    ) {
-      let markdown = res.data.Markdown;
-      this.setState({
-        contentHTML: markdown.contentHTML,
-        contentMarkdown: markdown.contentMarkdown,
-        description: markdown.description,
-        hasOldData: true,
-      });
-    } else {
-      this.setState({
-        contentHTML: "",
-        contentMarkdown: "",
-        description: "",
-        hasOldData: false,
-      });
-    }
-    console.log("res doctor : ", res);
-  };
-  handleOnchangeDescription = (event) => {
-    this.setState({
-      description: event.target.value,
-    });
-  };
-  handleSaveContentMarkdown = () => {
-    let {
-      contentHTML,
-      contentMarkdown,
-      description,
-      selectedOption,
-      hasOldData,
-    } = this.state;
-    if (
-      contentHTML &&
-      contentMarkdown &&
-      description &&
-      selectedOption &&
-      selectedOption.value
-    ) {
-      this.props.saveDetailDoctorAction({
-        contentHTML: contentHTML,
-        contentMarkdown: contentMarkdown,
-        description: description,
-        doctorId: selectedOption.value,
-        action: hasOldData === true ? manageActions.EDIT : manageActions.CREATE,
-      });
-    } else {
-      alert("Vui lòng điền thông tin");
-    }
-  };
-  render() {
-    let { selectedOption, description, hasOldData } = this.state;
-    //console.log("state : ", this.state);
-    return (
-      <div className="manage-doctor-container">
-        <div className="manage-doctor-title"> create information</div>
-        <div className="more-info">
-          <div className="content-left">
-            <label>choice doctor</label>
-            <Select
-              value={selectedOption}
-              onChange={this.handleChangeSelect}
-              options={this.state.listDoctor}
-            />
-          </div>
-          <div className="content-right">
-            <label>introduction info</label>
-            <textarea
-              className="form-control"
-              rows="4"
-              onChange={(event) => this.handleOnchangeDescription(event)}
-              value={description}
-            >
-              ThachNhe
-            </textarea>
-          </div>
-        </div>
-        <div className="manage-doctor-editor">
-          <MdEditor
-            style={{ height: "500px" }}
-            renderHTML={(text) => mdParser.render(text)}
-            onChange={this.handleEditorChange}
-            value={this.state.contentMarkdown}
-          />
-        </div>
-        <button
-          className={
-            hasOldData === true
-              ? "save-content-doctor"
-              : "create-content-doctor"
-          }
-          onClick={() => this.handleSaveContentMarkdown()}
-        >
-          {hasOldData === true ? (
-            <span>Lưu thông tin</span>
-          ) : (
-            <span>Tạo thông tin</span>
-          )}
-        </button>
-      </div>
-    );
-  }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    language: state.app.language,
-    allDoctors: state.admin.allDoctors,
-  };
+    return {
+        language: state.app.language,
+        allDoctors: state.admin.allDoctors,
+        allRequiredDoctorInfo: state.admin.allRequiredDoctorInfo,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchAllDoctor: () => dispatch(actions.fetchAllDoctor()),
-    saveDetailDoctorAction: (data) =>
-      dispatch(actions.saveDetailDoctorAction(data)),
-  };
+    return {
+        fetchAllDoctor: () => dispatch(actions.fetchAllDoctor()),
+        saveDetailDoctorAction: (data) => dispatch(actions.saveDetailDoctorAction(data)),
+        getRequireDoctorInfo: () => dispatch(actions.getRequireDoctorInfo()),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageDoctor);
--- Reactjs/src/store/reducers/adminReducer.js
+++ Reactjs/src/store/reducers/adminReducer.js
@@ -1,127 +1,141 @@
-import actionTypes from "../actions/actionTypes";
+import actionTypes from '../actions/actionTypes';
 
 const initialState = {
-  isOpenModal: true,
-  isLoadingGender: false,
-  genders: [],
-  roles: [],
-  position: [],
-  users: [],
-  topDoctors: [],
-  allDoctors: [],
-  allScheduleTime: [],
+    isOpenModal: true,
+    isLoadingGender: false,
+    genders: [],
+    roles: [],
+    position: [],
+    users: [],
+    topDoctors: [],
+    allDoctors: [],
+    allScheduleTime: [],
+    allRequiredDoctorInfo: [],
 };
 
 const adminReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actionTypes.FETCH_GENDER_START:
-      state.isLoadingGender = true;
-      // console.log("start : ", action);
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_GENDER_SUCCESS:
-      state.genders = action.data;
-      state.isLoadingGender = false;
+    switch (action.type) {
+        case actionTypes.FETCH_GENDER_START:
+            state.isLoadingGender = true;
+            // console.log("start : ", action);
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_GENDER_SUCCESS:
+            state.genders = action.data;
+            state.isLoadingGender = false;
 
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_GENDER_FAILED:
-      state.isLoadingGender = false;
-      state.genders = [];
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_POSITION_SUCCESS:
-      state.position = action.data;
-      state.isLoadingGender = false;
-      // console.log("data position : ", state);
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_POSITION_FAILED:
-      state.isLoadingGender = false;
-      state.position = [];
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_ROLE_SUCCESS:
-      state.roles = action.data;
-      state.isLoadingGender = false;
-      // console.log("data position : ", state);
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_ROLE_FAILED:
-      state.roles = [];
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_ALL_USERS__SUCCESS:
-      state.users = action.users;
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_ALL_USERS__FAILED:
-      state.users = [];
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.EDIT_USER__SUCCESS:
-      state.isOpenModal = false;
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_TOP_DOCTOR__SUCCESS:
-      state.topDoctors = action.data;
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_TOP_DOCTOR__FAILED:
-      state.topDoctors = [];
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_ALL_DOCTOR__SUCCESS:
-      state.allDoctors = action.data;
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_ALL_DOCTOR__FAILED:
-      state.allDoctors = [];
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_SUCCESS:
-      state.allScheduleTime = action.data;
-      return {
-        ...state,
-        started: true,
-      };
-    case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILED:
-      state.allScheduleTime = [];
-      return {
-        ...state,
-        started: true,
-      };
-    default:
-      return state;
-  }
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_GENDER_FAILED:
+            state.isLoadingGender = false;
+            state.genders = [];
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_POSITION_SUCCESS:
+            state.position = action.data;
+            state.isLoadingGender = false;
+            // console.log("data position : ", state);
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_POSITION_FAILED:
+            state.isLoadingGender = false;
+            state.position = [];
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_ROLE_SUCCESS:
+            state.roles = action.data;
+            state.isLoadingGender = false;
+            // console.log("data position : ", state);
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_ROLE_FAILED:
+            state.roles = [];
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_ALL_USERS__SUCCESS:
+            state.users = action.users;
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_ALL_USERS__FAILED:
+            state.users = [];
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.EDIT_USER__SUCCESS:
+            state.isOpenModal = false;
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_TOP_DOCTOR__SUCCESS:
+            state.topDoctors = action.data;
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_TOP_DOCTOR__FAILED:
+            state.topDoctors = [];
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_ALL_DOCTOR__SUCCESS:
+            state.allDoctors = action.data;
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_ALL_DOCTOR__FAILED:
+            state.allDoctors = [];
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_SUCCESS:
+            state.allScheduleTime = action.data;
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILED:
+            state.allScheduleTime = [];
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_REQUIRED_DOCTOR_INFO_SUCCESS:
+            state.allRequiredDoctorInfo = action.data;
+            // console.log('all required doctor info : ', state.allRequiredDoctorInfo);
+            return {
+                ...state,
+                started: true,
+            };
+        case actionTypes.FETCH_REQUIRED_DOCTOR_INFO_FAILED:
+            state.allRequiredDoctorInfo = [];
+            return {
+                ...state,
+                started: true,
+            };
+        default:
+            return state;
+    }
 };
 
 export default adminReducer;
